62bc0624acbb925a652e2edf5080815dbf9d12a3
Alexis Limozin <alexis@limozin.net>
2023-12-20 22:17:50 +0100

    First iteration of NMPC, not working

--- Deliverable_6_1/Deliverable_6_1.m
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -5,3 +5,26 @@ addpath(fullfile('..', 'src'));
 %clc
 
 %% TODO: This file should produce all the plots for the deliverable
+Ts = 1/20;
+rocket = Rocket(Ts);
+H = 5; % Horizon length in seconds
+nmpc = NmpcControl(rocket, H);
+
+% MPC reference with default maximum roll = 15 deg
+%ref = @(t_, x_) ref_TVC(t_);
+ref = [2; 2; 2; 0];
+% MPC reference with specified maximum roll = 50 deg
+roll_max = deg2rad(50);
+%ref = @(t_, x_) ref_TVC(t_, roll_max);
+
+% Evaluate once and plot optimal open−loop trajectory,
+% pad last input to get consistent size with time and state
+[u, T_opt, X_opt, U_opt] = nmpc.get_u(x, ref);
+U_opt(:,end+1) = nan;
+ph = rocket.plotvis(T_opt, X_opt, U_opt, ref);
+
+Tf = 5;
+x0 = [deg2rad([2 -2 0, -2 2 0]), 0 0 0, 0 0 0]';
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
+rocket.anim_rate = 1.0;
+rocket.vis(T,X,U);
---
+++ Deliverable_6_1/NmpcControl.m
@@ -0,0 +1,237 @@
+classdef NmpcControl < handle
+    
+    properties
+        solver
+        nx, nu, N
+        nlp_x0
+        nlp_lbx, nlp_ubx
+        nlp_lbg, nlp_ubg
+        nlp_p
+        
+        T_opt
+        sol
+        idx
+        
+        % Delay compensation
+        rocket
+        expected_delay
+        mem_u
+        
+        % Warmstart
+        nlp_lam_x0
+        nlp_lam_g0
+    end
+    
+    methods
+        function obj = NmpcControl(rocket, tf)
+           
+            import casadi.*
+            
+            N_segs = ceil(tf/rocket.Ts); % MPC horizon
+            nx = 12; % Number of states
+            nu = 4;  % Number of inputs
+            
+            % Decision variables (symbolic)
+            N = N_segs + 1; % Index of last point
+            X_sym = SX.sym('X_sym', nx, N); % state trajectory
+            U_sym = SX.sym('U_sym', nu, N-1); % control trajectory)
+            
+            % Parameters (symbolic)
+            x0_sym  = SX.sym('x0_sym', nx, 1);  % initial state
+            ref_sym = SX.sym('ref_sym', 4, 1);  % target position
+            
+            % Default state and input constraints (take 10000 instead of
+            % inf)
+            ubx = ones(nx, 1)*10000;
+            ubx(4) = deg2rad(75); % Default state constraint for alpha
+            ubx(5) = deg2rad(75); % Default state constraint for beta
+            ubx(5) = deg2rad(15); % Default state constraint for gamma
+            lbx = -ones(nx, 1)*10000;
+            lbx(4) = deg2rad(-75); % Default state constraint for alpha
+            lbx(5) = deg2rad(-75); % Default state constraint for beta
+            lbx(5) = deg2rad(-15); % Default state constraint for gamma
+            ubu = [deg2rad(15); deg2rad(15); 80; 20];
+            lbu = [deg2rad(-15); deg2rad(-15); 20; -20];
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % Q and R hyperparameters for cost. Need to tune later.
+            Q = eye(nx);
+            R = eye(nu);
+
+            % The reference state where we want to be. No acceleration, and
+            % at the target position.
+            X_ref = [SX.zeros(5,1); ref_sym(4,1); SX.zeros(3,1); ref_sym(1:3,1)];
+
+            % Cost
+            cost = (X_sym(:,1)-X_ref(:,1))'*Q*(X_sym(:,1)-X_ref(:,1))...
+                + U_sym(:,1)'*R*U_sym(:,1);
+            for i = 2:N_segs-1
+                cost = cost + ...
+                (X_sym(:,i)-X_ref(:,1))'*Q*(X_sym(:,i)-X_ref(:,1))...
+                + U_sym(:,i)'*R*U_sym(:,i);
+            end
+            % Equality constraints (Casadi SX), each entry == 0
+            eq_constr = X_sym(:,1) - x0_sym;
+            
+            % Inequality constraints (Casadi SX), each entry <= 0
+            ineq_constr = SX.zeros(nx*N*2+nu*(N-1)*2,1);
+            for i=1:N-1
+                ineq_constr((i-1)*(2*nx+2*nu)+1:i*(2*nx+2*nu), 1) = ...
+                    [X_sym(:,i) - ubx; ...
+                    lbx - X_sym(:,i); ...
+                    U_sym(:,i) - ubu; ...
+                    lbu - U_sym(:,i)];
+            end
+            ineq_constr((N-1)*(2*nx+2*nu)+1:nx*N*2+nu*(N-1)*2, 1) = ...
+                [X_sym(:,N) - ubx; ...
+                lbx - X_sym(:,N)];
+            % For box constraints on state and input, overwrite entries of
+            % lbx, ubx, lbu, ubu defined above
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % ---- Assemble NLP ------
+            nlp_x = [X_sym(:); U_sym(:)];
+            nlp_p = [x0_sym; ref_sym];
+            nlp_f = cost;
+            nlp_g = [eq_constr; ineq_constr];
+            
+            nlp = struct('x', nlp_x, 'p', nlp_p, 'f', nlp_f, 'g', nlp_g);
+            
+            % ---- Setup solver ------
+            opts = struct('ipopt', struct('print_level', 0), 'print_time', false);
+            obj.solver = nlpsol('solver', 'ipopt', nlp, opts);
+            
+            % ---- Assemble NLP bounds ----
+            obj.nlp_x0  = zeros(size(nlp_x));
+            
+            obj.nlp_ubx = [repmat(ubx, N, 1); repmat(ubu, (N-1), 1)];
+            obj.nlp_lbx = [repmat(lbx, N, 1); repmat(lbu, (N-1), 1)];
+            
+            obj.nlp_ubg = [zeros(size(eq_constr)); zeros(size(ineq_constr))];
+            obj.nlp_lbg = [zeros(size(eq_constr)); -inf(size(ineq_constr))];
+            
+            obj.nlp_p = [zeros(size(x0_sym)); zeros(size(ref_sym))];
+            
+            obj.nlp_lam_x0 = [];
+            obj.nlp_lam_g0 = [];
+            
+            obj.nx = nx;
+            obj.nu = nu;
+            obj.N = N;
+            obj.T_opt = linspace(0, N * rocket.Ts, N);
+            
+            obj.idx.X = [1, obj.N * obj.nx];
+            obj.idx.U = obj.idx.X(2) + [1, (obj.N-1) * obj.nu];
+            obj.idx.u0 = obj.idx.U(1) + [0, obj.nu-1];
+            
+            expected_delay = 0; % Need to change later
+            % Members for delay compensation
+            obj.rocket = rocket;
+            obj.expected_delay = expected_delay;
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            
+            u_init = [0; 0; 50; 0]; 
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            obj.mem_u = repmat(u_init, 1, expected_delay);
+        end
+        
+        function [u, T_opt, X_opt, U_opt] = get_u(obj, x0, ref)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            delay = obj.expected_delay;
+            mem_u = obj.mem_u;
+            
+            % Delay compensation: Predict x0 delay timesteps later.
+            % Simulate x_ for 'delay' timesteps
+            x_ = x0;
+            % ...
+       
+            x0 = x_;
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Compute solution from x0
+            obj.solve(x0, ref);
+            
+            % Evaluate u0
+            nlp_x = obj.sol.x;
+            id = obj.idx.u0;
+            u = full( nlp_x(id(1):id(2)) );      
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % Delay compensation: Save current u
+            if obj.expected_delay > 0
+               % obj.mem_u = ...
+            end
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            if nargout > 1, T_opt = obj.get_T_opt(); end
+            if nargout > 2, X_opt = obj.get_X_opt(); end
+            if nargout > 3, U_opt = obj.get_U_opt(); end
+            return
+            
+            % Additional evaluation
+            % Complete trajectory
+            % % X_opt = full(reshape(nlp_x(idx_X(1):idx_X(2)), obj.nx, obj.N));
+            % % U_opt = full(reshape(nlp_x(idx_U(1):idx_U(2)), obj.nu, obj.N - 1));
+            % %
+            % % cost_opt = full(sol.f);
+            % % constr_opt = full(sol.g);
+            % %
+            % % stats = obj.solver.stats;
+        end
+        
+        function solve(obj, x0, ref)
+            
+            % ---- Set the initial state and reference ----
+            obj.nlp_p = [x0; ref];     % Initial condition
+            obj.nlp_x0(1:obj.nx) = x0; % Initial guess consistent
+            
+            % ---- Solve the optimization problem ----
+            args = {'x0', obj.nlp_x0, ...
+                'lbg', obj.nlp_lbg, ...
+                'ubg', obj.nlp_ubg, ...
+                'lbx', obj.nlp_lbx, ...
+                'ubx', obj.nlp_ubx, ...
+                'p', obj.nlp_p, ...
+                %                 'lam_x0', obj.nlp_lam_x0, ...
+                %                 'lam_g0', obj.nlp_lam_g0
+                };
+            
+            obj.sol = obj.solver(args{:});
+            if obj.solver.stats.success ~= true
+                solve_status_str = obj.solver.stats.return_status;
+                fprintf([' [' class(obj) ': ' solve_status_str '] ']);
+                obj.sol.x(obj.idx.u0) = nan;
+            end
+            
+            % Use the current solution to speed up the next optimization
+            obj.nlp_x0 = obj.sol.x;
+            obj.nlp_lam_x0 = obj.sol.lam_x;
+            obj.nlp_lam_g0 = obj.sol.lam_g;
+        end
+        function T_opt = get_T_opt(obj)
+            T_opt = obj.T_opt;
+        end
+        function X_opt = get_X_opt(obj)
+            nlp_x = obj.sol.x;
+            id = obj.idx.X;
+            X_opt = full(reshape(nlp_x(id(1):id(2)), obj.nx, obj.N));
+        end
+        function U_opt = get_U_opt(obj)
+            nlp_x = obj.sol.x;
+            id = obj.idx.U;
+            U_opt = full(reshape(nlp_x(id(1):id(2)), obj.nu, obj.N - 1));
+        end
+    end
+end
+
