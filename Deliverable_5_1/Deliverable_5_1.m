0cab3fffc8de39087deef4d07e1250da0acb5e35
Alexis Limozin <alexis@limozin.net>
2024-01-10 16:25:11 +0100

    fixed 5.1

--- Deliverable_5_1/Deliverable_5_1.m
+++ Deliverable_5_1/Deliverable_5_1.m
@@ -30,17 +30,19 @@ rocket.mass = 2.13;
 x0 = [zeros(1, 9), 1 0 3]';
 ref = [1.2, 0, 3, 0]';
 
-%% Simulation and plot with estimator
-[T_est, X_est, U_est, Ref_est, Z_hat] = ...
-    rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
- 
+%% Simulation and plot without estimator
+
+[T_no_est, X_no_est, U_no_est, Ref_no_est] = ...
+    rocket.simulate(x0, Tf, @mpc.get_u, ref);
+
 ph_no_est = rocket.plotvis(T_no_est,X_no_est,U_no_est,Ref_no_est);
 ph_no_est.fig.Name = 'Merged lin. MPC in nonlinear simulation without estimator';
 saveas(gcf,'5.1 without estimator.png')
 
-%% Simulation and plot without estimator
-[T_no_est, X_no_est, U_no_est, Ref_no_est] = ...
-    rocket.simulate(x0, Tf, @mpc.get_u, ref);
+%% Simulation and plot with estimator
+[T_est, X_est, U_est, Ref_est, Z_hat] = ...
+    rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
+
 ph_est = rocket.plotvis(T_est,X_est,U_est,Ref_est);
 ph_est.fig.Name = 'Merged lin. MPC in nonlinear simulation with estimator';
 saveas(gcf,'5.1 with estimator.png')
